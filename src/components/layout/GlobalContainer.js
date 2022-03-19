ff3a0640a7cf0b5abe698415db5a4f14cd6b57a7
Joseph Zhang <jxsephz@gmail.com>
2022-03-19 11:09:34 -0400

    more tailwind changes

--- src/components/layout/GlobalContainer.js
+++ src/components/layout/GlobalContainer.js
@@ -1,5 +1,7 @@
 import styles from "../../pages/index.module.css";
 
 export default function GlobalContainer(props) {
-  return <div className={styles.globalContainer}>{props.children}</div>;
+  return (
+    <div className="h-screen grid grid-rows-[auto_1fr]">{props.children}</div>
+  );
 }
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,12 +1,11 @@
 import React from "react";
 import { motion } from "framer-motion";
-import styles from "./Navbar.module.css";
 
 export default function FontCard(props) {
   return (
     <motion.div
       // style={{ borderBottom: "1px solid black" }}
-      className="grid px-4 py-5 grid-cols-2 border-b-1 border-gray-900"
+      className="grid px-4 py-5 grid-cols-2 border-b sm:border-solid border-black "
     >
       {/* <motion.div className={styles.container}> */}
       <p className="flex justify-self-start"> Font Preference</p>
