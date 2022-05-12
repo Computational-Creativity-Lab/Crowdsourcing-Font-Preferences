917dc291a90221446e5414c8b0c31906bee5d7d0
Joseph Zhang <50648374+josephz-me@users.noreply.github.com>
2022-05-12 13:48:09 -0400

    add lottie animation

--- src/components/JoyRide.js
+++ src/components/JoyRide.js
@@ -1,49 +1,60 @@
 import { motion, AnimatePresence } from "framer-motion";
 import React, { useState } from "react";
 import Router from "next/router";
+import Lottie from "react-lottie";
+import onboardAnim from "../../public/onboarding-animation.json";
+
+const onboardingAnimSettings = {
+  loop: true,
+  autoplay: true,
+  animationData: onboardAnim,
+  rendererSettings: {
+    preserveAspectRatio: "xMidYMid slice",
+  },
+};
 
 export default function JoyRide(props) {
   return (
-    <>
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
-      >
-        <div className="absolute rounded-xl drop-shadow-2xl w-[800px] h-[500px] bg-zinc-900 z-20 p-8 left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%]">
-          <p className="text-4xl text-zinc-100 mb-10">
-            For each{" "}
-            <span className="py-2 px-4 border border-solid border-zinc-100 rounded-full">
-              keyword
-            </span>{" "}
-            select a font
-          </p>
-          <div className="text-white grid grid-cols-2 h-auto">
-            <div className="flex flex-row">
-              <p className="text-base">
-                You’ll have four chances to decide on the best match for that
-                keyword
-              </p>
-              <button
-                onClick={() => {
-                  Router.push("/survey");
-                  console.log("go to survey");
-                }}
-                className="p-2 bg-[rgba(255,255,255,.05)] rounded-full text-white"
-              >
-                Got it!
-              </button>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      className="absolute top-0 w-full h-full  bg-[rgba(0,0,0,.5)]"
+    >
+      <div>
+        <div className="absolute rounded-xl drop-shadow-2xl max-w-[800px] w-[90%] bg-zinc-900 z-20 left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%]">
+          <div className="px-8 pt-8">
+            <p className="leading-8 text-3xl text-zinc-100 mb-10">
+              Select the font that best matches each{" "}
+              <span className="pb-1 px-4 border border-solid border-zinc-100 rounded-full">
+                Keyword
+              </span>
+            </p>
+            <div className="text-white grid grid-cols-2 h-auto gap-8">
+              <div className="flex flex-col">
+                <p className="text-base">
+                  {`Whatever font you choose will remain. The unselected one will
+                  change. You’ll have four chances to decide on the best match
+                  for that keyword. After four rounds, the font that remains
+                  will be your selection and you’ll move on to a new keyword.`}
+                </p>
+              </div>
+
+              <div className="w-full h-auto">
+                <Lottie options={onboardingAnimSettings} />
+              </div>
             </div>
-            <div className="w-full h-[200px] bg-red-400"></div>
           </div>
+          <button
+            onClick={() => {
+              Router.push("/survey");
+            }}
+            className="w-full px-2 py-6 hover:bg-[rgba(255,255,255,.05)] border-t border-solid border-[rgba(255,255,255,.2)] text-white mt-8"
+          >
+            Begin
+          </button>
         </div>
-      </motion.div>
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
-        className="absolute bg-[rgba(0,0,0,.5)] w-full h-full z-10"
-      ></motion.div>
-    </>
+      </div>
+    </motion.div>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+
 import Navbar from "../components/layout/Navbar";
 import { motion, AnimatePresence } from "framer-motion";
 import HeadComp from "../components/HeadComp";
@@ -9,6 +10,10 @@ import JoyRide from "../components/JoyRide";
 
 export default function Home() {
   const [intro, showIntro] = useState(false);
+
+  useEffect(() => {
+    console.log(intro);
+  }, [intro]);
   useEffect(() => {
     // Make sure we are on client side
     if (typeof window !== "undefined") {
@@ -27,51 +32,7 @@ export default function Home() {
           showIntro(true);
         }}
       >
-        {intro && (
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            className="absolute top-0 w-full h-full  bg-[rgba(0,0,0,.5)]"
-          >
-            <motion.div
-              initial={{ opacity: 0, scale: 0.9 }}
-              animate={{ opacity: 1, scale: 1 }}
-              transition={{ ease: [0.16, 1, 0.3, 1], duration: 0.5 }}
-            >
-              <div className="absolute rounded-xl drop-shadow-2xl max-w-[800px] w-[90%] bg-zinc-900 z-20 left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%]">
-                <div className="px-8 pt-8">
-                  <p className="leading-8 text-3xl text-zinc-100 mb-10">
-                    Select the font that best matches each{" "}
-                    <span className="pb-1 px-4 border border-solid border-zinc-100 rounded-full">
-                      Keyword
-                    </span>
-                  </p>
-                  <div className="text-white grid grid-cols-2 h-auto gap-8">
-                    <div className="flex flex-col">
-                      <p className="text-base">
-                        Whatever font you choose will remain. The unselected one
-                        will change. You’ll have four chances to decide on the
-                        best match for that keyword. After four rounds, the font
-                        that remains will be your selection and you’ll move on
-                        to a new keyword.
-                      </p>
-                    </div>
-                    <div className="w-full h-[200px] bg-red-400"></div>
-                  </div>
-                </div>
-                <button
-                  onClick={() => {
-                    Router.push("/survey");
-                  }}
-                  className="w-full px-2 py-6 hover:bg-[rgba(255,255,255,.05)] border-t border-solid border-[rgba(255,255,255,.2)] text-white mt-8"
-                >
-                  Begin
-                </button>
-              </div>
-            </motion.div>
-          </motion.div>
-        )}
+        {intro && <JoyRide></JoyRide>}
         <AnimatePresence>{!intro && <CustomCursor />}</AnimatePresence>
         <HeadComp />
         <GlobalContainer>
