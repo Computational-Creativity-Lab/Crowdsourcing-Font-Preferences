eca90e59d878e6c082a12403c67b3c57c107e6cc
Mia Tang <miatang13@gmail.com>
2022-06-02 13:44:29 -0400

    Add documentation to shader background comp

--- src/components/fiberbg/Scene.js
+++ src/components/fiberbg/Scene.js
@@ -8,14 +8,16 @@ import { KEYWORDS, WAVE_INTENSITY } from "../../utils/settings";
 import gsap from "gsap";
 
 function Scene(props) {
+  // determine what textures to load depending on the user's device resolution 
   var textureUrls = [];
   var textures = {};
   var baseUrl = window.innerWidth <= 480 ? "/textures/mobile/" : "textures/";
-  console.log(baseUrl);
   for (let i = 0; i < KEYWORDS.length; i++) {
     textureUrls.push(baseUrl + KEYWORDS[i] + ".png");
     textures[KEYWORDS[i]] = undefined;
   }
+
+  // we need to do it like thise since useTexture is a hook from drei 
   const [map0, map1, map2, map3, map4, map5, map6, map7, map8, map9] =
     useTexture(textureUrls);
   const loadedMaps = [
@@ -34,10 +36,14 @@ function Scene(props) {
     textures[KEYWORDS[i]] = loadedMaps[i];
   }
 
+  // not currently used in production but can be fun to experiment with 
   const noiseMap = useTexture("/textures/Noise.png");
 
+  // the main state we keep track of, which allows us to switch textures and lerp between
+  // previous and current textures
   const [currentMap, setCurrentMap] = useState(textures[props.keyword]);
 
+  // these are done in the way fiber likes
   const ref = useRef();
   useFrame((state) => (data.uniforms.u_time.value = state.clock.elapsedTime));
 
@@ -60,6 +66,7 @@ function Scene(props) {
     []
   );
 
+  // we wish to update the current texture and start lerp whenever the keyword changes
   useEffect(() => {
     console.log("Shader prop:", props);
     data.uniforms.TexturePrev.value = currentMap;
@@ -80,6 +87,7 @@ function Scene(props) {
     });
   }, [props.keyword]);
 
+  // this is used to prototype and tweak the wave intensity, not used in production
   useEffect(() => {
     data.uniforms.u_wave_intensity.value = props.waveIntensity;
   }, [props.waveIntensity]);
